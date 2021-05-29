4a70b3c21bd26377b40f83429fcb733c231a7517
raptoravis <raptoravis@qq.com>
2021-05-29 23:10:19 +0800

    embedlua test added

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 #include "LuaScript.h"
 
-int main() {
+void luascript_test() {
     LuaScript script("Player.lua");
     float posX = script.get<float>("player.position.x");
     float posY = script.get<float>("player.position.y");
@@ -33,4 +33,48 @@ int main() {
             std::cout<<*it<<",";
     }
     std::cout<<std::endl;
+}
+
+/* 
+* define a function that returns version information to lua scripts
+*/
+static int hostgetversion(lua_State *l)
+{
+	/* Push the return values */
+	lua_pushnumber(l, 0);
+	lua_pushnumber(l, 99);
+	lua_pushnumber(l, 32);
+
+	/* Return the count of return values */
+	return 3;
+}
+
+void embedlua_test ()
+{
+	lua_State *l = luaL_newstate();
+	luaL_openlibs(l);
+
+	/* register host API for script */
+	lua_register(l, "hostgetversion", hostgetversion);
+
+	/* load script */
+	luaL_dofile(l, "mood.lua");
+
+	/* call mood() provided by script */
+	lua_getglobal(l, "mood");
+	lua_pushboolean(l, 1);
+	lua_call(l, 1, 1);
+	printf("The mood is %s\n", lua_tostring(l, -1));
+	lua_pop(l, 1);
+
+	lua_close(l);
+}
+
+
+int main() {
+	luascript_test();
+
+	embedlua_test();
+
+	return 0;
 }
---
+++ mood.lua
@@ -0,0 +1,19 @@
+--mood.lua
+
+-- Get version information from host
+major, minor, build = hostgetversion()
+print( "The host version is ", major, minor, build)
+print("The Lua interpreter version is ", _VERSION)
+
+-- Define a function for host to call
+function mood( b )
+
+    -- return a mood conditional on parameter
+    if (b and major > 0) then
+        return 'mood-happy'
+    elseif (major == 0) then
+        return 'mood-confused'
+    else
+        return 'mood-sad'
+    end
+end
