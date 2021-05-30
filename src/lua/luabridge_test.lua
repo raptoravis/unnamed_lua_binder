fc9b100cc3622038315fef5ffb4f34bd957da36d
raptoravis <raptoravis@qq.com>
2021-05-30 14:23:42 +0800

    lua bridge table

--- src/lua/luabridge_test.lua
+++ src/lua/luabridge_test.lua
@@ -2,3 +2,10 @@
 
 testString = "LuaBridge works!"
 number = 42
+
+window = {
+    title = "Window v.0.1",
+    width = 400,
+    height = 500
+}
+
--- src/luabridge.cpp
+++ src/luabridge.cpp
@@ -15,14 +15,43 @@ void luabridge_test(const char* lua_file) {
 	}
 	else {
 		luaL_openlibs(L);
-
 		lua_pcall(L, 0, 0, 0);
+
 		luabridge::LuaRef s = luabridge::getGlobal(L, "testString");
+		
+		if (s.isNil() || !s.isString()) {
+ 		   std::cout << "Variable not found!" << std::endl;
+		} else {
+			std::string luaString = s.cast<std::string>();
+			std::cout << luaString << std::endl;
+		}
+		
 		luabridge::LuaRef n = luabridge::getGlobal(L, "number");
-		std::string luaString = s.cast<std::string>();
-		int answer = n.cast<int>();
-		std::cout << luaString << std::endl;
-		std::cout << "And here's our number:" << answer << std::endl;
+
+		if (n.isNil() || !n.isNumber()) {
+ 		   std::cout << "Variable not found!" << std::endl;
+		} else {
+			int answer = n.cast<int>();
+			std::cout << "And here's our number:" << answer << std::endl;
+		}
+
+		luabridge::LuaRef t = luabridge::getGlobal(L, "window");
+
+		if (t.isNil() || !t.isTable()) {
+			std::cout << "Variable not found!" << std::endl;
+		} else {
+			luabridge::LuaRef title = t["title"];
+			luabridge::LuaRef w = t["width"];
+			luabridge::LuaRef h = t["height"];
+
+			std::string titleString = title.cast<std::string>();
+			int width = w.cast<int>();
+			int height = h.cast<int>();
+
+			std::cout << titleString << std::endl;
+			std::cout << "width = " << width << std::endl;
+			std::cout << "height = " << height << std::endl;
+		}
 	}
 
 	lua_close(L);
