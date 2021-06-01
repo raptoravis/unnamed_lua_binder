f008c5faecaf2efa98a0734858c55387f69fcc37
liyonggang <liyonggang@ztgame.com>
2021-06-01 11:40:43 +0800

    middleclass private class test

--- src/lua/middleclass_test.lua
+++ src/lua/middleclass_test.lua
@@ -7,3 +7,10 @@ require(current_folder .. 'middleclass_test_class')
 _internalClassCounter = 4 -- Attempt to modify the _internalClassCounter variable
 
 print(middleclass_test_class:getCount()) -- prints "0" .. 
+
+peter = middleclass_test_class:new('peter')
+peter:shout() -- My name is peter and I have a secret. You will never know it!
+
+-- print(_secretMethod(peter)) -- throws an error - _secretMethod is nil here.
+
+
--- src/lua/middleclass_test_class.lua
+++ src/lua/middleclass_test_class.lua
@@ -8,12 +8,29 @@ middleclass_test_class = class('middleclass_test_class')
 
 local _internalClassCounter = 0
 
-function middleclass_test_class:initialize()
+function middleclass_test_class:initialize(name)
   _internalClassCounter = _internalClassCounter + 1
+  self.name = name
   self.publicStuff = 2
 end
 
 function middleclass_test_class:getCount()
   return(_internalClassCounter)
+end
+
+local _secretMethod = function(self) -- notice the 'local' at the beginning, the = function and explicit self parameter
+  return( 'My name is ' .. self.name .. ' and I have a secret.' )
+end
+
+function middleclass_test_class:shout()
+  print( _secretMethod(self) .. ' You will never know it!' )
+end
+
+local _secretClassMethod = function() -- self parameter out
+  return( 'My name is ' .. MyClass3.name .. ' and I have a secret.' ) -- use MyClass3 directly.
+end
+
+function middleclass_test_class.classMethod(theClass)
+  return( 'Being a public class named ' .. theClass.name .. ' is not a bad thing.' )
 end
 
