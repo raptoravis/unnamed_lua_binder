6bee34a2119cc01415db6459ba55c8f94d282f7e
liyonggang <liyonggang@ztgame.com>
2021-06-01 11:49:49 +0800

    use the local scope for middle class test

--- src/lua/middleclass_test.lua
+++ src/lua/middleclass_test.lua
@@ -13,4 +13,22 @@ peter:shout() -- My name is peter and I have a secret. You will never know it!
 
 -- print(_secretMethod(peter)) -- throws an error - _secretMethod is nil here.
 
+stewie = middleclass_test_class:new('stewie', 2, 'male')
 
+print(stewie:getName()) -- stewie
+stewie.name = 'ann'
+print(stewie.name) -- ann
+
+print(stewie:getAge()) -- 2
+stewie.age = 14        -- this isn't really modifying the age... it is creating a new public attribute called 'age'
+-- the internal age is still unaltered
+print(stewie:getAge()) -- 2
+-- the newly created external age is also available.
+print(stewie.age) -- 14
+
+-- same goes with gender:
+
+print(stewie:getGender()) -- 'male'
+stewie.gender = 'female'
+print(stewie:getGender()) -- 'male'
+print(stewie.gender) -- 'female'
--- src/lua/middleclass_test_class.lua
+++ src/lua/middleclass_test_class.lua
@@ -6,24 +6,33 @@ local class = require(current_folder .. 'lovetoys.lib.middleclass')
 
 middleclass_test_class = class('middleclass_test_class')
 
+local _private = setmetatable({}, {__mode = "k"})   -- weak table storing all private attributes
+
 local _internalClassCounter = 0
 
-function middleclass_test_class:initialize(name)
+function middleclass_test_class:initialize(name, age, gender)
   _internalClassCounter = _internalClassCounter + 1
   self.name = name
   self.publicStuff = 2
+
+  _private[self] = {
+    age = age,
+    gender = gender
+  }
 end
 
 function middleclass_test_class:getCount()
   return(_internalClassCounter)
 end
 
-local _secretMethod = function(self) -- notice the 'local' at the beginning, the = function and explicit self parameter
-  return( 'My name is ' .. self.name .. ' and I have a secret.' )
-end
+do
+  local _secretMethod = function(self) -- notice the 'local' at the beginning, the = function and explicit self parameter
+    return( 'My name is ' .. self.name .. ' and I have a secret.' )
+  end
 
-function middleclass_test_class:shout()
-  print( _secretMethod(self) .. ' You will never know it!' )
+  function middleclass_test_class:shout()
+    print( _secretMethod(self) .. ' You will never know it!' )
+  end
 end
 
 local _secretClassMethod = function() -- self parameter out
@@ -32,5 +41,17 @@ end
 
 function middleclass_test_class.classMethod(theClass)
   return( 'Being a public class named ' .. theClass.name .. ' is not a bad thing.' )
+end
+
+function middleclass_test_class:getName() -- shorter equivalent: MyClass4:getter('name')
+  return self.name
+end
+
+function middleclass_test_class:getAge()
+  return _private[self].age
+end
+
+function middleclass_test_class:getGender()
+  return _private[self].gender
 end
 
